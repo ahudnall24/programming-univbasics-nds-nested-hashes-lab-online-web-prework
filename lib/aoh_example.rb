cc536cdc40aa8b291354ccd938f362f12bc17df7
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-27 06:05:04 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -44,6 +44,9 @@ end
 
 def literal_aoh
   Infinite_Jest=[{:name=>"Don Gately", :occupation=>"Live-in-Staff"},
+ { :name  "Joelle van Dyne", :occupation "Radio Personality"},
+ {name:  "Pat Monteseian",occupation: "Staff"
+ 
   ]
   
   # Using Array literal syntax only, build a nested array that uses the data in
